f15b3cba2cd2c056335a046342673742c376711e
Vinícius Tavares Mota <106537834+viniciustmota@users.noreply.github.com>
2023-09-06 15:19:38 -0300

    Add files via upload

---
+++ Relogio/Relogio/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Relogio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnDesligar = new System.Windows.Forms.Button();
+            this.txtHora = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnLigar = new System.Windows.Forms.Button();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDesligar
+            // 
+            this.btnDesligar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesligar.Location = new System.Drawing.Point(319, 255);
+            this.btnDesligar.Name = "btnDesligar";
+            this.btnDesligar.Size = new System.Drawing.Size(163, 42);
+            this.btnDesligar.TabIndex = 0;
+            this.btnDesligar.Text = "DESLIGAR";
+            this.btnDesligar.UseVisualStyleBackColor = true;
+            this.btnDesligar.Click += new System.EventHandler(this.BtnDesligar_Click);
+            // 
+            // txtHora
+            // 
+            this.txtHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHora.Location = new System.Drawing.Point(268, 126);
+            this.txtHora.Name = "txtHora";
+            this.txtHora.ReadOnly = true;
+            this.txtHora.Size = new System.Drawing.Size(270, 29);
+            this.txtHora.TabIndex = 1;
+            this.txtHora.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // btnLigar
+            // 
+            this.btnLigar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLigar.Location = new System.Drawing.Point(319, 199);
+            this.btnLigar.Name = "btnLigar";
+            this.btnLigar.Size = new System.Drawing.Size(163, 42);
+            this.btnLigar.TabIndex = 2;
+            this.btnLigar.Text = "LIGAR";
+            this.btnLigar.UseVisualStyleBackColor = true;
+            this.btnLigar.Click += new System.EventHandler(this.BtnLigar_Click);
+            // 
+            // txtData
+            // 
+            this.txtData.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtData.Location = new System.Drawing.Point(268, 75);
+            this.txtData.Name = "txtData";
+            this.txtData.ReadOnly = true;
+            this.txtData.Size = new System.Drawing.Size(270, 29);
+            this.txtData.TabIndex = 3;
+            this.txtData.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.btnLigar);
+            this.Controls.Add(this.txtHora);
+            this.Controls.Add(this.btnDesligar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDesligar;
+        private System.Windows.Forms.TextBox txtHora;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnLigar;
+        private System.Windows.Forms.TextBox txtData;
+    }
+}
+
---
+++ Relogio/Relogio/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Relogio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLigar_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void BtnDesligar_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            txtData.Text = "";
+            txtHora.Text = "";
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            txtData.Text = System.DateTime.Today.ToShortDateString();
+            txtHora.Text = System.DateTime.Now.ToLongTimeString();
+        }
+    }
+}
